842248e79d7b32e7b4e2063247e073f3b8ee41a4
chenyonglei <37016501+cylhah@users.noreply.github.com>
2022-06-13 20:00:45 +0800

    feat: 修改高度

--- src/electron-main/index.ts
+++ src/electron-main/index.ts
@@ -8,7 +8,7 @@ function createWindow() {
 
     const win = new BrowserWindow({
         width: 800,
-        height: 600,
+        height: 300,
         webPreferences: {
             devTools: true,
             contextIsolation: false, // 是否开启隔离上下文
